cc03e0c486c0869a124acf4e669c5e5eb87cec3e
DanielWidhi <danielwihdi98@gmail.com>
2024-10-02 09:31:26 +0800

    first commit

---
+++ app/controllers/About.php
@@ -0,0 +1,13 @@
+<?php
+
+class About
+{
+    public function index($nama = 'Daniel Widhi', $pekerjaan = 'Foto')
+    {
+        echo 'Halo, Nama Saya $nama, Saya Seorang $pekerjaan';
+    }
+    public function page()
+    {
+        echo 'About/page';
+    }
+}
