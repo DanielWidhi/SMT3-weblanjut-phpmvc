68f2f16ce6670122396fb8c5be7f2356adb5a93d
DanielWidhi <danielwihdi98@gmail.com>
2024-10-28 22:04:20 +0800

    angjay

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -142,7 +142,7 @@
         <div class="card-body">
             <h5 class="card-title">Naren Jatuh Cinta</h5>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <a href="<?=BASEURL;?>/about" class="btn btn-primary">Go somewhere</a>
         </div>
     </div>
     <div class="card" style="width: 18rem;">
